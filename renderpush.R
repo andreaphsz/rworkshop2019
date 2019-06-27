fac842c243d1e6c822bd7e9ee229dc30ecfa99e7
andreaphsz <andrea.cantieni@phsz.ch>
2019-06-27 15:39:40 +0200

    dev

--- renderpush.R
+++ renderpush.R
@@ -1,8 +1,8 @@
 source("mycred.R")
 
 rmarkdown::render("slides.Rmd")
-cat("commit")
+cat("commit\r")
 git2r::commit(message="dev", all=TRUE)
-cat("push")
+cat("push\r")
 git2r::push(credentials=cred)
-cat("finished")
+cat("finished\r")
