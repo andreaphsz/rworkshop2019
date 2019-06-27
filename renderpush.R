dca9a71693da7aa9e85d301c1f81887e2d0ae528
andreaphsz <andrea.cantieni@phsz.ch>
2019-06-27 15:43:58 +0200

    dev

--- renderpush.R
+++ renderpush.R
@@ -1,7 +1,7 @@
 source("mycred.R")
 
 rmarkdown::render("slides.Rmd")
-message("\rcommiting...")
+message("committing...")
 git2r::commit(message="dev", all=TRUE)
 message("pushing...")
 git2r::push(credentials=cred)
